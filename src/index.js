609c996724ea2bc6ba0845085c43f5b43383bfcd
JesthelJhoy <jesthel.jhoy@gmail.com>
2025-04-10 17:14:16 +0200

    Added JS to update real timezone

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
+function updateTime() {
+  let milanElement = document.querySelector("#milan");
+  let milanDateElement = milanElement.querySelector(".date");
+  let milanTimeElement = milanElement.querySelector(".time");
+  let milanTime = moment().tz("Europe/Rome");
 
+  milanDateElement.innerHTML = milanTime.format("MMMM Do YYYY");
+  milanTimeElement.innerHTML = milanTime.format("HH:mm:ss A");
+
+  // london
+
+  let londonElement = document.querySelector("#london");
+  let londonDateElement = londonElement.querySelector(".date");
+  let londonTimeElement = londonElement.querySelector(".time");
+  let londonTime = moment().tz("Europe/London");
+
+  londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY");
+  londonTimeElement.innerHTML = londonTime.format("HH:mm:ss A");
+
+  // Tokyo
+
+  let tokyoElement = document.querySelector("#tokyo");
+  let tokyoDateElement = tokyoElement.querySelector(".date");
+  let tokyoTimeElement = tokyoElement.querySelector(".time");
+  let tokyoTime = moment().tz("Asia/Tokyo");
+
+  tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
+  tokyoTimeElement.innerHTML = tokyoTime.format("HH:mm:ss A");
+}
+
+updateTime();
+setInterval(updateTime, 1000);
